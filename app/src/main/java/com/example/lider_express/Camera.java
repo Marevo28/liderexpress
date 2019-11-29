275df02f13934a7dfb6d9bd31b73f4d416cf36bf
Ruslan <ruslanbarinov.spb@gmail.com>
2019-11-29 15:56:51 +0300

    Brightness V1

--- app/src/main/java/com/example/lider_express/Camera.java
+++ app/src/main/java/com/example/lider_express/Camera.java
@@ -22,13 +22,16 @@ import android.os.Environment;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.util.Log;
+import android.util.Range;
 import android.util.Size;
 import android.util.SparseIntArray;
 import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
@@ -60,8 +63,12 @@ public class Camera extends AppCompatActivity {
 
     private Button mButtonTakePhoto = null;
     private ToggleButton mButtonFlash = null;
+    private ToggleButton mButtonBrightness = null;
     private boolean mFlashFlag = false;
-    private Button mButtonBrightness = null;
+    private boolean mFlashFlagCount = false;
+    private FrameLayout mFrameLayoutSeekBar = null;
+    private SeekBar seekBar = null;
+    private boolean mBrightnessFlagCount = true;
     private TextureView mTextureView = null;
     private HandlerThread mBackgroundThread;
     private Handler mBackgroundHandler = null;
@@ -94,7 +101,10 @@ public class Camera extends AppCompatActivity {
 
         mButtonTakePhoto =  findViewById(R.id.btnCapture);
         mButtonFlash =  findViewById(R.id.btnFlash);
-        mButtonBrightness =findViewById(R.id.btnBrightness);
+        mButtonBrightness = findViewById(R.id.btnBrightness);
+        mFrameLayoutSeekBar = findViewById(R.id.frameLayoutSeekBar);
+        mFrameLayoutSeekBar.setVisibility(View.INVISIBLE);
+        seekBar = findViewById(R.id.seekBar);
         mTextureView = findViewById(R.id.textureView);
 
         mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
@@ -126,13 +136,39 @@ public class Camera extends AppCompatActivity {
             public void onClick(View view){
                 if(mButtonFlash.isChecked()){
                     mFlashFlag = true;
+
                 }else{
                     mFlashFlag = false;
+                }
+                mFlashFlagCount = true;
+            }
+        });
+
+        mButtonBrightness.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(mButtonBrightness.isChecked()){
+                    mFrameLayoutSeekBar.setVisibility(View.VISIBLE);
+                    mBrightnessFlagCount = false;
+                    myCameras.setBrightness(seekBar.getProgress());
+                }else{
+                    mFrameLayoutSeekBar.setVisibility(View.INVISIBLE);
+                    mBrightnessFlagCount = true;
+                    myCameras.setBrightness(0);
                 }
             }
         });
 
-
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                myCameras.setBrightness(progress);
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) { }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
 
         Bundle arguments = getIntent().getExtras();
         Papka = arguments.getString("Papka");
@@ -165,9 +201,9 @@ public class Camera extends AppCompatActivity {
         private ImageReader mImageReader;
         private int mImageWidth;
         private int mImageHeight;
-        private CaptureRequest.Builder builder;
-
         private CaptureRequest.Builder mBuilder;
+        private int minCompensationRange = 0;
+        private int maxCompensationRange = 10;
 
         public CameraService(CameraManager cameraManager, String cameraID) {
             mCameraManager = cameraManager;
@@ -182,12 +218,20 @@ public class Camera extends AppCompatActivity {
 
                 captureBuilder.addTarget(mImageReader.getSurface());
 
-                if(mFlashFlag) {
-                    builder.set(CaptureRequest.FLASH_MODE, CaptureRequest.FLASH_MODE_SINGLE);
-                    mCaptureSession.setRepeatingRequest(builder.build(), null, null);
-                }else{
-                    builder.set(CaptureRequest.FLASH_MODE, CaptureRequest.FLASH_MODE_OFF);
-                    mCaptureSession.setRepeatingRequest(builder.build(), null, null);
+                if(mFlashFlagCount) {
+                    if (mFlashFlag) {
+                        mBuilder.set(CaptureRequest.FLASH_MODE, CaptureRequest.FLASH_MODE_SINGLE);
+                        mCaptureSession.setRepeatingRequest(mBuilder.build(), null, null);
+                        mFlashFlagCount = false;
+                    } else {
+                        mBuilder.set(CaptureRequest.FLASH_MODE, CaptureRequest.FLASH_MODE_OFF);
+                        mCaptureSession.setRepeatingRequest(mBuilder.build(), null, null);
+                        mFlashFlagCount = false;
+                    }
+                }
+
+                if(mBrightnessFlagCount) {
+                    captureBuilder.set(CaptureRequest.CONTROL_AE_TARGET_FPS_RANGE, getRange());
                 }
 
                 int rotation = getWindowManager().getDefaultDisplay().getRotation();
@@ -257,8 +301,6 @@ public class Camera extends AppCompatActivity {
 
         /** ============================== createCameraPreviewSession ========================= **/
         private void createCameraPreviewSession() {
-
-
             mImageWidth = MainActivity.getDisplayWidth();
             mImageHeight = MainActivity.getDisplayHeight();
 
@@ -272,9 +314,11 @@ public class Camera extends AppCompatActivity {
             Surface surface = new Surface(texture);
 
             try {
-
-                builder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-                builder.addTarget(surface);
+                mBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+                mBuilder.addTarget(surface);
+                if(mBrightnessFlagCount) {
+                    mBuilder.set(CaptureRequest.CONTROL_AE_TARGET_FPS_RANGE, getRange());
+                }
                 mCameraDevice.createCaptureSession(Arrays.asList(surface,mImageReader.getSurface()),
                         new CameraCaptureSession.StateCallback() {
                             // =========================================
@@ -282,7 +326,7 @@ public class Camera extends AppCompatActivity {
                             public void onConfigured(CameraCaptureSession session) {
                                 mCaptureSession = session;
                                 try {
-                                    mCaptureSession.setRepeatingRequest(builder.build(),null,mBackgroundHandler);
+                                    mCaptureSession.setRepeatingRequest(mBuilder.build(),null,mBackgroundHandler);
                                 } catch (CameraAccessException e) {
                                     e.printStackTrace();
                                 }
@@ -299,6 +343,43 @@ public class Camera extends AppCompatActivity {
 
         } // --- createCameraPreviewSession ---
         /** ------------------------------ createCameraPreviewSession ------------------------- **/
+
+        /** ==================================== Range ======================================== **/
+        private Range<Integer> getRange() {
+            CameraCharacteristics chars = null;
+            try {
+                chars = mCameraManager.getCameraCharacteristics(cameraID);
+                Range<Integer>[] ranges = chars.get(CameraCharacteristics.CONTROL_AE_AVAILABLE_TARGET_FPS_RANGES);
+                Range<Integer> result = null;
+                for (Range<Integer> range : ranges) {
+                    int upper = range.getUpper();
+                    // 10 - min range upper for my needs
+                    if (upper >= 10) {
+                        if (result == null || upper < result.getUpper().intValue()) {
+                            result = range;
+                        }
+                    }
+                }
+                if (result == null) {
+                    result = ranges[0];
+                }
+                return result;
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+        /** ------------------------------------------ Range ----------------------------------- **/
+
+        public void setBrightness(int value) {
+            int brightness = (int) (minCompensationRange + (maxCompensationRange - minCompensationRange) * (value / 100f));
+            mBuilder.set(CaptureRequest.CONTROL_AE_EXPOSURE_COMPENSATION, brightness);
+            try {
+                mCaptureSession.setRepeatingRequest(mBuilder.build(), null, null);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
 
 
         public boolean isOpen() {
